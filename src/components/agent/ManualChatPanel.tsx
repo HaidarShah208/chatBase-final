56c669456ec3553e273e7d6ff47ad2d529059d5b
Ali Haider <alhi7896542@gmail.com>
2026-03-31 18:56:29 +0500

    fix menual chat width height

--- src/components/agent/ManualChatPanel.tsx
+++ src/components/agent/ManualChatPanel.tsx
@@ -1,10 +1,12 @@
-import { MessageSquare, Sparkle, } from 'lucide-react'
+import { MessageSquare, Sparkle } from 'lucide-react'
+import { Button } from '../ui/Button'
 
 export function ManualChatPanel() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-(--white)">
-      <div className="mx-auto flex w-full max-w-[420px] flex-col gap-4 lg:mx-0">
-        <div className="flex flex-col min-h-[220px] items-center rounded-2xl border border-(--border) bg-(--white) px-6 py-7 text-center shadow-sm">
+      <div className="flex h-full py-8 px-4 w-full flex-col gap-4 lg:mx-0">
+
+        <div className="flex flex-1 flex-col items-center rounded-2xl border border-(--border) bg-(--white) px-6 py-7 text-center ">
           <span className="flex h-13 w-13 items-center justify-center rounded-full bg-(--background)">
             <MessageSquare className="h-7 w-7 text-(--grayish)" aria-hidden />
           </span>
@@ -14,20 +16,21 @@ export function ManualChatPanel() {
           </p>
         </div>
 
-        <div className="flex flex-col items-center rounded-2xl border border-(--border) bg-(--white) px-5 py-6 shadow-sm">
-          <div className="w-full max-w-[360px] text-left">
+        <div className="flex flex-1 flex-col items-center rounded-2xl border border-(--border) bg-(--white) px-5 py-6">
+          <div className="w-full text-left">
             <p className="text-sm text-(--black)">User Prompt</p>
             <input
               className="mt-3 w-full rounded-lg border border-(--border) bg-(--white) px-3 py-3 text-xs text-(--black) outline-none placeholder:text-(--grayish)"
               placeholder="You are a customer who wants to return a package..."
             />
-            <button
+            <Button
               type="button"
+              variant='primary'
               className="mt-4 inline-flex h-10 items-center justify-center rounded-lg bg-(--brand) px-4 text-xs font-semibold text-(--white) shadow-sm transition hover:brightness-[0.98]"
             >
-              <Sparkle className="mr-1.5 h-3.5 w-3.5" aria-hidden />
+              <Sparkle className=" h-3.5 w-3.5" aria-hidden />
               Simulate Conversation
-            </button>
+            </Button>
           </div>
 
           <span className="mt-8 flex h-12 w-12 items-center justify-center rounded-full bg-(--background)">
@@ -39,9 +42,7 @@ export function ManualChatPanel() {
           </p>
         </div>
 
-        
       </div>
     </div>
   )
 }
-
